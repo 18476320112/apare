a0d0f97c83475d226cdc31dcfdd111df2d039fa2
guoweiguang <1217212146@qq.com>
2019-06-23 21:10:16 +0800

    优化前进, 后退, 分页功能

--- plugin/marquee/marquee.js
+++ plugin/marquee/marquee.js
@@ -12,13 +12,16 @@
 (function($) {
     var Marquee = function(el, options) {
         this.$el = el;
-        this.currentIndex = 0;
+        this.currentIndex = 1;
         // 合并配置
         this.options = $.extend({}, Marquee.DEFAULTS, options);
         this.init();
     };
     // 设置默认数据
     Marquee.DEFAULTS = {
+        // 延迟时间
+        delay: 700,
+        // 滑动速度
         speed: 1700,
         // 默认自动播放
         autoplay: true,
@@ -33,71 +36,93 @@
         // 初始化事件
         this.bindEvents();
         // 
-        this.autoMove();
+        // this.autoMove();
     };
     // 初始化事件
     Marquee.prototype.bindEvents = function() {
         var _this = this;
-        $('#marquee').on('click', '.pagination li', function() {
+        // 点击分页
+        _this.$el.on('click', '.pagination li', function() {
             // 停止自动轮播
             clearTimeout(_this.timer);
             // 防止重复点击
             if (_this.flag) {
                 return false
-            }
-            _this.move($(this).index())
+            };
+            _this.currentIndex = $(this).index() + 1;
+            _this.move();
+        }).on('mouseenter', function() {
+            $('.prev').show();
+            $('.next').show();
+        }).on('mouseleave', function() {
+            $('.prev').hide();
+            $('.next').hide();
+        }).on('click', '.next', function() { // 下一页 
+            // 防止重复点击
+            if (_this.flag) {
+                return false
+            };
+            _this.currentIndex += 1;
+            _this.move();
+        }).on('click', '.prev', function() { // 上一页
+            // 防止重复点击
+            if (_this.flag) {
+                return false
+            };
+            _this.currentIndex -= 1;
+            _this.move();
         })
     };
     // 执行动画
-    Marquee.prototype.move = function(idx) {
+    Marquee.prototype.move = function() {
         var _this = this;
         _this.flag = true;
-        // 获取index值
-        _this.currentIndex = idx;
-        if (idx == _this.timeLen) {
-            $('.pagination li').eq(0).addClass('active').siblings().removeClass('active');
-            var animateWidth = idx * _this.baseWdith;
-            _this.$el.find('.marquee-wrapper').animate({ left: -animateWidth + 'px' }, _this.options.speed, function() {
-                _this.flag = false;
-                _this.$el.find('.marquee-wrapper').css({ 'left': 0 });
-            });
-            _this.currentIndex = 0;
-        } else {
-            // 移动的长度
-            var animateWidth = idx * _this.baseWdith;
-            $('.pagination li').eq(idx).addClass('active').siblings().removeClass('active');
-            _this.$el.find('.marquee-wrapper').animate({ left: -animateWidth + 'px' }, _this.options.speed, function() {
-                _this.flag = false
-            });
-        }
-        _this.autoMove();
+        // 移动的长度
+        var animateWidth = _this.currentIndex * _this.baseWdith;
+        clearTimeout(_this.timerSub);
+        // 分页延迟
+        _this.timerSub = setTimeout(function() {
+            if (_this.currentIndex === _this.timeLen) { // 是否为最后一页
+                $('.pagination li').eq(0).addClass('active').siblings().removeClass('active');
+            } else { // 是否为第一页
+                $('.pagination li').eq(_this.currentIndex - 1).addClass('active').siblings().removeClass('active');
+            }
+        }, _this.options.speed / 2);
+
+        _this.$el.find('.marquee-wrapper').animate({ left: -animateWidth + 'px' }, _this.options.speed, function() {
+            _this.flag = false;
+            if (_this.currentIndex === _this.timeLen) { // 是否为最后一页
+                _this.$el.find('.marquee-wrapper').css({ 'left': -_this.baseWdith * 1 + 'px' });
+                _this.currentIndex = 1;
+            } else if (_this.currentIndex === 0) { // 是否为第一页
+                _this.$el.find('.marquee-wrapper').css({ 'left': -_this.baseWdith * (_this.timeLen - 1) + 'px' });
+                _this.currentIndex = _this.timeLen - 1;
+            }
+        });
     };
     // 
     Marquee.prototype.autoMove = function() {
         var _this = this;
-        // 防止重复点击
-        if (_this.flag) {
-            return false
-        }
         clearTimeout(this.timer);
         _this.timer = setTimeout(function() {
             _this.move(++_this.currentIndex);
-        }, _this.options.speed);
+        }, _this.options.speed + _this.options.delay);
     };
     // 初始化页面
     Marquee.prototype.renderDome = function() {
         // 滚动元素个数
-        this.timeLen = this.$el.find('.marquee-wrapper li').length;
-        // 复制第一个元素用于无缝滚动
-        this.$el.find('.marquee-wrapper li').eq(0).clone().appendTo('.marquee-wrapper');
+        this.timeLen = this.$el.find('.marquee-item').length + 1;
+        // 复制第一个和最后一个元素用于无缝滚动
+        this.$el.find('.marquee-item').eq(0).clone().appendTo('.marquee-wrapper');
+        this.$el.find('.marquee-item').eq(this.timeLen - 2).clone().prependTo('.marquee-wrapper');
         // 计算容器的宽度
         this.baseWdith = this.$el.width();
         // 子容器宽度赋值
-        this.$el.find('.marquee-wrapper').width(this.baseWdith * (this.timeLen + 1));
+        this.$el.find('.marquee-wrapper').width(this.baseWdith * (this.timeLen + 1)).css({ 'left': -this.baseWdith + 'px' });
         // 分页渲染
         if (this.options.paginationer) {
             var paginaStr = '<div class="pagination">';
-            for (var i = 0; i < this.timeLen; i++) {
+            for (var i = 0; i < this.timeLen - 1; i++) {
                 if (i === 0) {
                     paginaStr += '<li class="active"></li>'
                     continue;
